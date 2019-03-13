cb2b8a9192cfe0065473741491d78a9e4159bbb2
Thomas Hempel <thomas@scriptme.de>
2019-03-13 08:17:30 +0100

    Add Testing setup

---
+++ Tests/Unit/DemoTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lfda\Headless\Tests\Unit;
+
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class DemoTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     */
+    public function test_something_wrong()
+    {
+        $this->assertTrue(True, 'Some true assertion');
+    }
+
+    /**
+     * @test
+     */
+    public function test_something_correct()
+    {
+        $this->assertTrue(False, 'Some wrong assertion');
+    }
+
+}
